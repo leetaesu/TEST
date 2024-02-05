16d4685ff9973c26b5a11044a217e44274f89042
leetaesu <dlxotniocu@naver.com>
2024-02-06 08:42:30 +0900

    JS 누락

---
+++ game/src/assets/js/dist/common.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var windowSize = $(window).innerWidth(),
+    baseSize = 640;
+
+if (windowSize < baseSize) {
+  $(document).find('html').animate({
+    fontSize: $(window).innerWidth() * 10 / 36
+  }, 0);
+} else {
+  $(document).find('html').animate({
+    fontSize: baseSize * 10 / 36
+  }, 0);
+} // rem resize 수치 변환
+
+
+$(window).on('resize', function () {
+  if ($(window).innerWidth() < baseSize) {
+    $(document).find('html').animate({
+      fontSize: $(window).innerWidth() * 10 / 36
+    }, 0);
+  } else {
+    $(document).find('html').animate({
+      fontSize: baseSize * 10 / 36
+    }, 0);
+  }
+});
